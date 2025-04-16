d3d061cefd16ce717eb0bca3889d6d357a389e93
M1nks2 <caio1011sales@gmail.com>
2025-04-16 15:53:43 -0300

    correcoes

--- src/app/components/calculadora-component/calculadora-component.component.ts
+++ src/app/components/calculadora-component/calculadora-component.component.ts
@@ -11,22 +11,4 @@ export class CalculadoraComponent {
   valor1: number = 0;
   valor2: number = 0;
   operacao: string = 'soma';
-
-  calcular(): number | undefined {
-    if (this.operacao === 'divisao' && this.valor2 === 0) {
-      return undefined; // Retorna undefined em caso de divisão por zero
-    }
-    switch (this.operacao) {
-      case 'soma':
-        return this.valor1 + this.valor2;
-      case 'subtracao':
-        return this.valor1 - this.valor2;
-      case 'multiplicacao':
-        return this.valor1 * this.valor2;
-      case 'divisao':
-        return this.valor1 / this.valor2;
-      default:
-        return undefined;
-    }
-  }
 }
